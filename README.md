1e82210e788c62ef62f9b5f2eb60f9d28f900684
Adrian <hadrian3689@gmail.com>
2023-06-15 23:05:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Volatility Install
+The original post: https://seanthegeek.net/1172/how-to-install-volatility-2-and-volatility-3-on-debian-ubuntu-or-kali-linux/amp/
+
+## Getting Started
+Just **run** it as your current *user* or as *root*
+```
+bash install.sh
+```
+
+## Usage
+You can use **volatility 2** with:
+```
+vol.py -h
+```
+---
+You can use **volatility 3** with:
+```
+vol -h
+```
+
+## Disclaimer
+All the code provided on this repository is for educational/research purposes only. Any actions and/or activities related to the material contained within this repository is solely your responsibility. The misuse of the code in this repository can result in criminal charges brought against the persons in question. Author will not be held responsible in the event any criminal charges be brought against any individuals misusing the code in this repository to break the law.
---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+python2_setup () {
+        sudo apt-get update -y
+        sudo apt-get install -y build-essential git libdistorm3-dev yara libraw1394-11 libcapstone-dev capstone-tool tzdata
+        sudo apt-get install -y python2 python2.7-dev libpython2-dev
+}
+python3_setup () {
+        sudo apt-get install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools python3-wheel
+}
+volatility2 () {
+        USER=$(whoami)
+        curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+        python2 get-pip.py
+        rm get-pip.py
+        python2 -m pip install -U setuptools wheel
+        python2 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
+        python2 -m pip install yara
+        python2 -m pip install -U git+https://github.com/volatilityfoundation/volatility.git
+        sudo ln -s /home/$USER/.local/lib/python2.7/site-packages/usr/lib/libyara.so /usr/lib/libyara.so 
+}
+volatility3 () {
+        USER=$(whoami)
+        VERSION=$(python3 -V | awk '{print $2}' | grep -Po '^[0-9]+\.[0-9]+')
+        python3 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
+        python3 -m pip install -U git+https://github.com/volatilityfoundation/volatility3.git
+        sed -i 's/xrange/range/g' /home/$USER/.local/lib/python$VERSION/site-packages/Crypto/Cipher/ARC4.py
+}
+python2_setup
+python3_setup
+volatility2
+volatility3
